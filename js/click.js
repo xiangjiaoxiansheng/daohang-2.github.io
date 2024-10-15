1ecb8a023691e6f301a9d7719a0253262aa3531c
cano <ellenzhao888888@gmail.com>
2024-10-15 10:33:39 +0800

    fix

---
+++ js/click.js
@@ -0,0 +1,25 @@
+document.getElementById("btn-1").onclick = function () {
+    window.location.href = links.address1;
+};
+document.getElementById("btn-2").onclick = function () {
+    window.location.href = links.address2;
+};
+document.getElementById("btn-3").onclick = function () {
+    window.location.href = links.address3;
+};
+document.getElementById("btn-4").onclick = function () {
+    window.location.href = links.address4;
+};
+document.getElementById("btn-5").onclick = function () {
+    window.location.href = links.address5;
+};
+document.getElementById("btn-6").onclick = function () {
+    window.location.href = links.address6;
+};
+document.getElementById("btn-7").onclick = function () {
+    window.location.href = links.address7;
+};
+document.getElementById("btn-8").onclick = function () {
+    window.location.href = links.address8;
+};
+
---
+++ js/links.js
@@ -0,0 +1,10 @@
+const links = {
+    address1: 'https://xj13.xyz',
+    address2: 'https://xj222.xyz',
+    address3: 'https://xj32.xyz',
+    address4: 'https://xj33.xyz',
+    address5: 'https://xj521.xyz',
+    address6: 'https://ysge.win',
+    address7: 'https://yxge.app',
+    address8: 'https://ysge.cc',
+};
