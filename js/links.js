98e50062b75fc1668ed89ae565bbe97daa9c05eb
xiangjiaoxiansheng <ccaqqqw@gmail.com>
2024-10-26 12:38:58 +0800

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,10 +1,10 @@
 const links = {
-    address1: 'https://xj13.xyz',
+    address1: 'https://www.xjxs9.app',
     address2: 'https://xj222.xyz',
-    address3: 'https://xj32.xyz',
-    address4: 'https://xj33.xyz',
+    address3: 'https://www.xjxs8.app',
+    address4: 'https://xjxs7.app',
     address5: 'https://xj521.xyz',
-    address6: 'https://ysge.win',
-    address7: 'https://yxge.app',
-    address8: 'https://ysge.cc',
+    address6: 'https://xjxs6.app',
+    address7: 'https://www.xjxs5.com',
+    address8: 'https://www.xjxs5.app',
 };
